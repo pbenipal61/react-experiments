ed1b5a1ae2943d37b313563fbf5a6dbe028b4942
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-06-07 16:18:57 +0300

    Firebase exps

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,62 @@ import ChildHub from "./Hub/ChildHub";
 import Fingerprint from "./Fingerprint";
 import LocalStorageExp from "./LocalStorageExp";
 import { storage } from "./utils";
-
+import FirebaseCExps from "./FirebaseCExps";
+import * as firebase from 'firebase/app';
+import 'firebase/analytics'
+import 'firebase/remote-config'
 function App() {
-    const [cmp, setCmp] = React.useState<any>();
-    const language = window.navigator.language;
-    window.navigator.geolocation.getCurrentPosition((position: any) => {
-       console.log(position);
+
+    const [color, setColor] = React.useState<string>();
+    const firebaseConfig = {
+      apiKey: "AIzaSyCDlAMzgV-zqTxlIkRiaiA2aFswxS1I4gc",
+      authDomain: "coresine-experiments.firebaseapp.com",
+      databaseURL: "https://coresine-experiments.firebaseio.com",
+      projectId: "coresine-experiments",
+      storageBucket: "coresine-experiments.appspot.com",
+      messagingSenderId: "335448367218",
+      appId: "1:335448367218:web:a9e7697b12589c389a62eb",
+      measurementId: "G-RJLXKF3YMS"
+    }
+
+    try{
+      firebase.initializeApp(firebaseConfig);
+      
+    }
+    catch(err){
+      if (!/already exists/.test(err.message)) {
+        console.error('Firebase initialization error raised', err.stack)
+      }
+    }
+
+    firebase.analytics()
+    
+    const remoteConfig = firebase.remoteConfig();
+    remoteConfig.settings = {
+      minimumFetchIntervalMillis: 3600000,
+      fetchTimeoutMillis: 2000,
+    };
+
+    remoteConfig.defaultConfig = ({
+      color: '#bf8e45'
     });
-    console.log(language)
+    
+    ( async () => {
+     try{
+      await remoteConfig.fetchAndActivate();
+      const colorStr = remoteConfig.getString('color');
+      console.log(colorStr);
+      setColor(colorStr);
+     }
+     catch(err){
+       console.log("error downloading remote config", err.stack);
+     }
+    })()
+
 
   return (
     <div className = {styles['wrapper']}>
-        <button onClick={() => {
-            storage.save("key", "some dope ass data");
-            setCmp(<Fingerprint />);
-        }}>Local storage exps</button>
-        {cmp}
+        <FirebaseCExps color={color?color:''}/>
     </div>
   );
 }
---
+++ src/FirebaseCExps.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FirebaseExps = ({ color }: { color: string }) => {
+    return <div style={{
+        backgroundColor: color
+    }}>FirebaseExps</div>
+}
+
+export default FirebaseExps;
